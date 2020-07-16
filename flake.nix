28d61cd662c69e24dd8bce50bdefc75a2886e88a
Timothy DeHerrera <tim.deh@pm.me>
2020-07-16 14:06:30 -0600

    enable jitsi, turn, and riot-web support

--- flake.nix
+++ flake.nix
@@ -7,7 +7,7 @@
     with nixpkgs.lib;
     let
       system = "x86_64-linux";
-      pkgs = import nixpkgs { inherit system; };
+      pkgs_ = import nixpkgs { inherit system; };
 
       region = "us-west-1";
       accessKeyId = fileContents ./secrets/keyid;
@@ -16,14 +16,16 @@
       database_pass = fileContents ./secrets/database;
     in {
       devShell."${system}" =
-        pkgs.mkShell { buildInputs = [ nixops.defaultPackage."${system}" ]; };
+        pkgs_.mkShell { buildInputs = [ nixops.defaultPackage."${system}" ]; };
 
       nixopsConfigurations = {
         default = {
           inherit nixpkgs;
           network.description = "Synapse Server";
 
-          synapse-server = { resources, ... }: {
+          synapse-server = { resources, pkgs, config, ... }: {
+            imports = [ ./modules/jitsi-meet.nix ];
+
             deployment = {
               targetEnv = "ec2";
               ec2 = {
@@ -33,6 +35,64 @@
                 keyPair = resources.ec2KeyPairs.nrdxp-keys;
                 elasticIPv4 = address;
               };
+            };
+
+            environment.systemPackages = with pkgs; [ riot-web ];
+            nixpkgs.overlays = [
+              (self: super: {
+                riot-web = super.riot-web.override {
+                  conf = {
+                    default_server_config = {
+                      "m.homeserver" = {
+                        "base_url" = "https://matrix.nrdxp.dev";
+                        "server_name" = "nrdxp.dev";
+                      };
+                      "m.identity_server" = {
+                        "base_url" = "https://vector.im";
+                      };
+                    };
+
+                    ## jitsi will be setup later,
+                    ## but we need to add to Riot configuration
+                    jitsi.preferredDomain = "jitsi.nrdxp.dev";
+                  };
+                };
+              })
+            ];
+
+            services.jitsi-meet = {
+              enable = true;
+              hostName = "jitsi.nrdxp.dev";
+            };
+
+            services.jitsi-videobridge.openFirewall = true;
+
+            services.coturn = {
+              enable = true;
+              use-auth-secret = true;
+              static-auth-secret = fileContents ./secrets/turn;
+              realm = "turn.nrdxp.dev";
+              no-tcp-relay = true;
+              extraConfig = ''
+                user-quota=12
+                total-quota=1200
+                denied-peer-ip=10.0.0.0-10.255.255.255
+                denied-peer-ip=192.168.0.0-192.168.255.255
+                denied-peer-ip=172.16.0.0-172.31.255.255
+
+                allowed-peer-ip=192.168.191.127
+
+                cipher-list="HIGH"
+                no-loopback-peers
+                no-multicast-peers
+              '';
+
+              secure-stun = true;
+              cert = "/var/lib/acme/turn.nrdxp.dev/fullchain.pem";
+              pkey = "/var/lib/acme/turn.nrdxp.dev/key.pem";
+
+              min-port = 49152;
+              max-port = 49999;
             };
 
             services.matrix-synapse = {
@@ -46,16 +106,41 @@
                 "/var/lib/acme/matrix.nrdxp.dev/fullchain.pem";
               tls_private_key_path = "/var/lib/acme/matrix.nrdxp.dev/key.pem";
               listeners = [
-                # client
-                {
+                { # federation
+                  bind_address = "";
+                  port = 8448;
+                  resources = [
+                    {
+                      compress = false;
+                      names = [ "federation" ];
+                    }
+                    {
+                      compress = true;
+                      names = [ "client" "webclient" ];
+                    }
+                  ];
+                  type = "http";
+
+                  tls = true;
+                  x_forwarded = false;
+                }
+                { # client
                   port = 8008;
                   resources = [{
                     compress = true;
-                    names = [ "client" "federation" ];
+                    names = [ "client" "webclient" ];
                   }];
                   tls = false;
                 }
               ];
+
+              turn_uris = [
+                "turn:turn.dangerousdemos.net:3478?transport=udp"
+                "turn:turn.dangerousdemos.net:3478?transport=tcp"
+              ];
+
+              turn_shared_secret = config.services.coturn.static-auth-secret;
+
               extraConfig = ''
                 max_upload_size: "10M"
               '';
@@ -75,6 +160,19 @@
                   enableACME = true;
                   locations."/" = { proxyPass = "http://127.0.0.1:8008"; };
                 };
+                "riot.nrdxp.dev" = {
+                  forceSSL = true;
+                  enableACME = true;
+                  locations."/" = { root = pkgs.riot-web; };
+                };
+                "${config.services.jitsi-meet.hostName}" = {
+                  enableACME = true;
+                  forceSSL = true;
+                };
+                "turn.nrdxp.dev" = {
+                  enableACME = true;
+                  forceSSL = true;
+                };
               };
             };
 
@@ -83,6 +181,13 @@
               email = "tim@nrdxp.dev";
               acceptTerms = true;
               certs = {
+                "turn.nrdxp.dev" = {
+                  group = "turnserver";
+                  allowKeysForGroup = true;
+                  postRun =
+                    "systemctl reload nginx.service; systemctl restart coturn.service";
+                };
+
                 "matrix.nrdxp.dev" = {
                   group = "matrix-synapse";
                   allowKeysForGroup = true;
@@ -103,14 +208,18 @@
               '';
             };
 
-            networking.firewall = {
+            networking.firewall = let
+              range = with config.services.coturn; {
+                from = min-port;
+                to = max-port;
+              };
+            in {
               enable = true;
-              allowedTCPPorts = [
-                22
-                8448 # Matrix federation
-                80
-                443
-              ];
+              allowedUDPPorts = [ 3478 5349 5350 ];
+              allowedUDPPortRanges = [ range ];
+              allowedTCPPortRanges = [ range ];
+
+              allowedTCPPorts = [ 22 80 443 3478 3479 8448 ];
             };
           };
 
